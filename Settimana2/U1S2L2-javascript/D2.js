3545b89e6e3a8ad20e905e280b0a446d544de0ef
GabScognamiglio <gabbo16s@gmail.com>
2024-01-30 15:53:58 +0100

    es7

--- Settimana2/U1S2L2-javascript/D2.js
+++ Settimana2/U1S2L2-javascript/D2.js
@@ -25,20 +25,20 @@ if (num3 !== 5) { console.log('not equal') }
 let num4 = 60
 let resto = num4 % 5
 
-if (resto===0) {console.log ('il resto è ' + resto + ' quindi ' + num4 + ' è divisibile per 5' )}
-else {console.log('numero non divisibile per 5')}
+if (resto === 0) { console.log('il resto è ' + resto + ' quindi ' + num4 + ' è divisibile per 5') }
+else { console.log('numero non divisibile per 5') }
 
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let num5=5
-let num6=3
+let num5 = 5
+let num6 = 3
 
-if (num5 === 8 || num6===8){console.log('uno dei due numeri è 8')}
-else if (num5+num6===8 || num5-num6===8 || num6-num5===8){console.log('l\'addizione/sottrazione dei due numeri equivale a 8')}
-else {console.log('nessuno dei due numeri equivale a 8, nè la loro sottrazione/addizione equivale a 8')}
+if (num5 === 8 || num6 === 8) { console.log('uno dei due numeri è 8') }
+else if (num5 + num6 === 8 || num5 - num6 === 8 || num6 - num5 === 8) { console.log('l\'addizione/sottrazione dei due numeri equivale a 8') }
+else { console.log('nessuno dei due numeri equivale a 8, nè la loro sottrazione/addizione equivale a 8') }
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -48,10 +48,10 @@ else {console.log('nessuno dei due numeri equivale a 8, nè la loro sottrazione/
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 let totalShoppingCart = 100
-let shippingCost= 10
+let shippingCost = 10
 
-if (totalShoppingCart <= 50){console.log ('il totale del carrello è ' + (totalShoppingCart + shippingCost) + ' euro, di cui ' + shippingCost + ' euro di spedizione')}
-else {console.log ('il totale del carrello è ' + totalShoppingCart + ', con spedizione gratuita!')}
+if (totalShoppingCart <= 50) { console.log('il totale del carrello è ' + (totalShoppingCart + shippingCost) + ' euro, di cui ' + shippingCost + ' euro di spedizione') }
+else { console.log('il totale del carrello è ' + totalShoppingCart + ', con spedizione gratuita!') }
 
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
@@ -60,11 +60,11 @@ else {console.log ('il totale del carrello è ' + totalShoppingCart + ', con spe
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 let day = 'BlackFriday'
-if (day==='BlackFriday'){totalShoppingCart*=0.8}
+if (day === 'BlackFriday') { totalShoppingCart *= 0.8 }
 console.log(totalShoppingCart)
 
-if (totalShoppingCart <= 50){console.log ('il totale del carrello è ' + (totalShoppingCart + shippingCost) + ' euro, di cui ' + shippingCost + ' euro di spedizione')}
-else {console.log ('il totale del carrello è ' + totalShoppingCart + ', con spedizione gratuita!')}
+if (totalShoppingCart <= 50) { console.log('il totale del carrello è ' + (totalShoppingCart + shippingCost) + ' euro, di cui ' + shippingCost + ' euro di spedizione') }
+else { console.log('il totale del carrello è ' + totalShoppingCart + ', con spedizione gratuita!') }
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -73,6 +73,25 @@ else {console.log ('il totale del carrello è ' + totalShoppingCart + ', con spe
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let var1 = 16
+let var2 = 95
+let var3 = 5
+console.log(var1, var2, var3)
+
+if (var1 < var2 && var1 < var3) { console.log(var1) }
+else if (var2 < var1 && var2 < var3) { console.log(var2) }  /*condizioni per far stampare la var più piccola*/
+else { console.log(var3) }
+
+if ((var1 < var2 && var1 > var3) || (var1 > var2 && var1 < var3)) { console.log(var1) }
+else if ((var2 < var1 && var2 > var3) || (var2 > var1 && var2 < var3)) { console.log(var2) }   /*condizioni per far stampare la var di mezzo*/
+else { console.log(var3) }
+
+if (var1 > var2 && var1 > var3) { console.log(var1) }
+else if (var2 > var1 && var2 > var3) { console.log(var2) }  /*condizioni per far stampare la var più grande*/
+else { console.log(var3) }
+
+
+
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
