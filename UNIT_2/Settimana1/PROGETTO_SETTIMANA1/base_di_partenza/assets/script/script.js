171edfcf3342f1683f730ae0b06fb5e0a9fe24df
GabScognamiglio <gabbo16s@gmail.com>
2024-02-23 14:46:30 +0100

    aggiunta animazione header

--- UNIT_2/Settimana1/PROGETTO_SETTIMANA1/base_di_partenza/assets/script/script.js
+++ UNIT_2/Settimana1/PROGETTO_SETTIMANA1/base_di_partenza/assets/script/script.js
@@ -2,9 +2,11 @@ const header= document.getElementById('header');
 const getStarted=document.getElementById('getStarted');
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 450) {
       header.style.backgroundColor = 'white';
+      getStarted.style.backgroundColor = '#538724';
     } else {
       header.style.backgroundColor = '#ffc017';
+      getStarted.style.backgroundColor = '#191919';
     }
   });
