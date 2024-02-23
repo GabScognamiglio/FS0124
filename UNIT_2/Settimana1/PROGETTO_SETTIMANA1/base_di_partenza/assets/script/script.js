f4e7aa7dfcb590ca4ed9fb7c385c200887e514c7
GabScognamiglio <gabbo16s@gmail.com>
2024-02-23 16:43:30 +0100

    formattazione documenti

--- UNIT_2/Settimana1/PROGETTO_SETTIMANA1/base_di_partenza/assets/script/script.js
+++ UNIT_2/Settimana1/PROGETTO_SETTIMANA1/base_di_partenza/assets/script/script.js
@@ -1,29 +1,29 @@
-const header= document.getElementById('header');
-const getStarted=document.getElementById('getStarted');
+const header = document.getElementById('header');
+const getStarted = document.getElementById('getStarted');
 const listaM = document.querySelectorAll("g[stroke-linecap=butt]");
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 430) {
-      header.style.backgroundColor = 'white';
-      getStarted.style.backgroundColor = '#538724';
-    } else {
-      header.style.backgroundColor = '#ffc017';
-      getStarted.style.backgroundColor = '#191919';
-    }
-  });
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 430) {
+    header.style.backgroundColor = 'white';
+    getStarted.style.backgroundColor = '#538724';
+  } else {
+    header.style.backgroundColor = '#ffc017';
+    getStarted.style.backgroundColor = '#191919';
+  }
+});
 
 
 
 function compariScompari() {
   let indiceLettera = Math.floor(Math.random() * listaM.length);
- if (listaM[indiceLettera].style.opacity==='0') {
-  listaM[indiceLettera].style.opacity='1'
- }
- else {
-  listaM[indiceLettera].style.opacity='0'
- }
+  if (listaM[indiceLettera].style.opacity === '0') {
+    listaM[indiceLettera].style.opacity = '1'
+  }
+  else {
+    listaM[indiceLettera].style.opacity = '0'
+  }
 }
 
-setInterval(compariScompari, 40); 
+setInterval(compariScompari, 40);
 
 
