7c39e54ba3733550115beb84d4d0cf7884ab08a7
GabScognamiglio <gabbo16s@gmail.com>
2024-03-04 16:09:08 +0100

    migliorie minori

--- UNIT_2/Settimana3/U2S3L1/es_giornaliero/assets/script/pets.js
+++ UNIT_2/Settimana3/U2S3L1/es_giornaliero/assets/script/pets.js
@@ -1,12 +1,11 @@
 const btn = document.getElementById('btn');
 const listAnimali = document.getElementById('listaAnimali');
-
-
 let nome = document.getElementById('nome');
 let proprietario = document.getElementById('proprietario');
 let specie = document.getElementById('specie');
 let razza = document.getElementById('razza');
-
+const errore = document.getElementById('errore');
+errore.style.display='none'
 let arrayAnimali =[]
 
 class Pets {
@@ -33,25 +32,26 @@ let cane2= new Pets ('zumbats', 'mario', 'bo', 'boh')
 
 console.log(cane1.stessoProprietario(cane2));
 
-
-
-
 btn.addEventListener('click', function (e) {
-    // e.preventDefault();
-
+    e.preventDefault();
+    //se i primi due campi non sono riempiti--> STOP
     if (nome.value=='' || proprietario.value==''){
+        errore.style.display='block'
         return;
+    }
+    else {
+        errore.style.display='none'
     };
-
+    //creo un'istanza della classe Pets dandogli i valori dell'input utente e lo pusho nell'array
     let nuovoAnimale = new Pets (nome.value, proprietario.value, specie.value, razza.value)
     arrayAnimali.push(nuovoAnimale);
     console.log(arrayAnimali);
-
+    //creo il list item, lo riempio e lo appendo
     let newLi=document.createElement('li');
     newLi.innerText=`${nuovoAnimale.petName}, di ${nuovoAnimale.ownerName}`
-    newLi.setAttribute('class', 'list-group-item')
+    newLi.setAttribute('class', 'list-group-item my-3')
     listAnimali.appendChild(newLi);
-
+    //svuoto i campi
     nome.value='';
     proprietario.value='';
     specie.value='';
