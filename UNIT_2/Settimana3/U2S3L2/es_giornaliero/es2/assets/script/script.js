568a2493e16421990c9458dd8fa348913b2941ea
GabScognamiglio <gabbo16s@gmail.com>
2024-03-05 15:52:33 +0100

    esercizi 1 e 2 completi

--- UNIT_2/Settimana3/U2S3L2/es_giornaliero/es2/assets/script/script.js
+++ UNIT_2/Settimana3/U2S3L2/es_giornaliero/es2/assets/script/script.js
@@ -1,1 +1,18 @@
 const timer = document.getElementById('timer');
+
+function iniziaTimer() {
+    let start = sessionStorage.getItem('start');  //cerco se esiste un session storage
+    if (!start) {
+      start = Date.now();
+      sessionStorage.setItem('start', start); //se non c'è lo creo
+    } 
+
+    setInterval(function() {
+      let tempoCorrente = Date.now();
+      let tempoPassato = Math.floor((tempoCorrente - start) / 1000);
+      timer.innerText = tempoPassato;
+    }, 1000);
+}
+
+window.onload = iniziaTimer;
+  
