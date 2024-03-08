469f8f81afdb8be8b2e5b390e601a8e4441e0a46
GabScognamiglio <gabbo16s@gmail.com>
2024-03-08 11:52:11 +0100

    2/3 pagine complete

---
+++ UNIT_2/Settimana3/PROGETTO_SETTIMANALE/assets/js/details.js
@@ -0,0 +1,49 @@
+const cardContainer = document.getElementById('cardContainer');
+const myKey = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZGNlMjJkN2IxMTAwMTkwZTZlMDQiLCJpYXQiOjE3MDk4OTEyNDEsImV4cCI6MTcxMTEwMDg0MX0.yMGLZ0AUI-8VOiWP4D785JUzrn4_zGLcZxQttST-duc';
+const dataURL = "https://striveschool-api.herokuapp.com/api/product/";
+const productImg = document.getElementById('productImg');
+const productDetails = document.getElementById('productDetails');
+
+
+
+const params = new URLSearchParams(location.search)
+const id = params.get('id')
+
+
+window.addEventListener('load', init);
+
+function init() {
+    loadDetails();
+    console.log(id)
+}
+
+async function loadDetails() {
+    try {
+        let response = await fetch((dataURL + id), {
+            headers: {
+                "Authorization": myKey
+            }
+        })
+        data = await response.json();
+        prodotto = data;
+        console.log(prodotto);
+        printDetails();
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+function printDetails() {
+    let img = document.createElement('img');
+    img.classList.add('img-fluid');
+    img.setAttribute('src', prodotto.imageUrl);
+    productImg.appendChild(img);
+
+    productDetails.innerHTML=`
+                    <p>${prodotto.brand}</p>
+                    <h3 class="fs-2 my-3">${prodotto.name}</h3>
+                    <p><span><samp class="btn btn-dark text-warning">${prodotto.price}€</samp></span></p>
+                    <p>${prodotto.description}</p>`
+
+}
---
+++ UNIT_2/Settimana3/PROGETTO_SETTIMANALE/assets/js/script.js
@@ -0,0 +1,50 @@
+const cardContainer = document.getElementById('cardContainer');
+const myKey = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZGNlMjJkN2IxMTAwMTkwZTZlMDQiLCJpYXQiOjE3MDk4OTEyNDEsImV4cCI6MTcxMTEwMDg0MX0.yMGLZ0AUI-8VOiWP4D785JUzrn4_zGLcZxQttST-duc';
+const dataURL = "https://striveschool-api.herokuapp.com/api/product/";
+prodotti = [];
+
+
+window.addEventListener('load', init);
+
+function init() {
+    loadProducts();
+}
+
+async function loadProducts() {
+    try {
+        let response = await fetch(dataURL, {
+            headers: {
+                "Authorization": myKey
+            }
+        })
+        data = await response.json();
+        prodotti = data;
+        console.log(prodotti);
+        printProducts();
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+function printProducts(){
+    cardContainer.innerHTML = '';
+    for (let i = 0; i < prodotti.length; i++) {
+        let div = document.createElement('div');
+        div.classList.add('col-lg-3');
+        div.classList.add('col-sm-6');
+        div.classList.add('col-md-4');
+        div.innerHTML = `
+        <div class="card" >
+                        <img src="${prodotti[i].imageUrl}" class="card-img-top img-fluid" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">${prodotti[i].brand}</p>
+                          <h5 class="card-title">${prodotti[i].name}</h5>
+                          <p class="card-text">${prodotti[i].description}</p>
+                          <p><a href="back.html" class="btn btn-warning">Modifica</a></p>
+                          <p><a href="http://127.0.0.1:5500/details.html?id=${prodotti[i]._id}" class="btn btn-secondary">Scopri di più</a></p>
+                        </div>
+                      </div>`
+        cardContainer.appendChild(div);
+    }
+}
