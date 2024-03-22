53fdca66890b02fcc96d6d8aa907a3b8905e068e
GabScognamiglio <gabbo16s@gmail.com>
2024-03-22 09:59:02 +0100

    creazione ambiente per progetto settimanale

--- UNIT_3/Settimana1/U3S1L4/es-giornaliero/src/app/components/post-details/post-details.component.ts
+++ UNIT_3/Settimana1/U3S1L4/es-giornaliero/src/app/components/post-details/post-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { Post } from 'src/app/models/post.interface';
 import { ActivatedRoute,} from '@angular/router';
 
@@ -9,28 +9,44 @@ import { ActivatedRoute,} from '@angular/router';
 })
 export class PostDetailsComponent {
   idCercato!: string|any;
-  numIdCercato!:number|any;
+  // numIdCercato!:number|any;
   post!:Post|any
   posts!:Post[]
 
+    // constructor(private route: ActivatedRoute) {
+  //   this.idCercato = this.route.snapshot.paramMap.get('id');
+  //   console.log(this.idCercato);
+  //   this.getDetails().then((data)=>{
+  //     console.log(data);
+  //     this.posts = data;
+  //     this.numIdCercato=parseInt(this.idCercato)
+  //     this.post = this.posts.find(oggetto => oggetto.id === this.numIdCercato)
+  //     console.log(this.post)
+  //   })
+  // }
+
+  // async getDetails() {
+  //   const response = await fetch('../../assets/db.json/');
+  //   const data = await response.json();
+  //   return data;
+  // }
+  
   constructor(private route: ActivatedRoute) {
     this.idCercato = this.route.snapshot.paramMap.get('id');
     console.log(this.idCercato);
     this.getDetails().then((data)=>{
       console.log(data);
-      this.posts = data;
-      this.numIdCercato=parseInt(this.idCercato)
-      this.post = this.posts.find(oggetto => oggetto.id === this.numIdCercato)
-      console.log(this.post)
+      this.post = data
     })
   }
 
-  ngOnInit() {}
-
-
   async getDetails() {
-    const response = await fetch('../../assets/db.json/');
+    const response = await fetch('http://localhost:3000/' + (parseInt(this.idCercato)-1));
     const data = await response.json();
     return data;
   }
 }
+
+
+
+
