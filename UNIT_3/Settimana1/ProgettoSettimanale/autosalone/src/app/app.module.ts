3858ce3036d16e513626245abd96b76e9bf6fecc
GabScognamiglio <gabbo16s@gmail.com>
2024-03-22 10:59:38 +0100

    nav e footer completi

--- UNIT_3/Settimana1/ProgettoSettimanale/autosalone/src/app/app.module.ts
+++ UNIT_3/Settimana1/ProgettoSettimanale/autosalone/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -9,6 +10,25 @@ import { AudiPageComponent } from './components/audi-page/audi-page.component';
 import { FordPageComponent } from './components/ford-page/ford-page.component';
 import { FiatPageComponent } from './components/fiat-page/fiat-page.component';
 
+
+const routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'audi',
+    component:AudiPageComponent
+  },
+  {
+    path:'ford',
+    component:FordPageComponent
+  },
+  {
+    path:'fiat',
+    component:FiatPageComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +40,8 @@ import { FiatPageComponent } from './components/fiat-page/fiat-page.component';
     FiatPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
