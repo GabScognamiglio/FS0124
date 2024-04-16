14227b7d12a2549bd91f944bb8f8cc07c6b80c97
GabScognamiglio <gabbo16s@gmail.com>
2024-04-16 12:14:20 +0200

    Ambiente es giornaliero java

---
+++ UNIT_4_JAVA/Settimana 1/U4W1D2/esGiornaliero/src/it/esercizi/Esercizio1.java
@@ -0,0 +1,14 @@
+package it.esercizi;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean stringaPariDispari(String stringa){
+        if (stringa.length()%2==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
