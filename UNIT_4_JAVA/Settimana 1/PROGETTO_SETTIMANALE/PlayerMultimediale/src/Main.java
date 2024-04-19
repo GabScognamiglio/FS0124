6995a318a0b74ea19fc081649f97b8b645953025
GabScognamiglio <gabbo16s@gmail.com>
2024-04-19 16:13:58 +0200

    progetto settimanale completo

--- UNIT_4_JAVA/Settimana 1/PROGETTO_SETTIMANALE/PlayerMultimediale/src/Main.java
+++ UNIT_4_JAVA/Settimana 1/PROGETTO_SETTIMANALE/PlayerMultimediale/src/Main.java
@@ -3,19 +3,75 @@ import player.Immagine;
 import player.RegistrazioneAudio;
 import player.Video;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//   FUNZIONA TUTTO
-//        ElementoMultimediale a1 = new RegistrazioneAudio("registrazione1", 3);
-//
-//        a1.riproduci();
-//
-//        ElementoMultimediale v1 = new Video("video1",7, 6);
-//
-//        v1.riproduci();
-//
-//        ElementoMultimediale i1 = new Immagine("immagine 1",3);
-//
-//        i1.riproduci();
+
+        ElementoMultimediale[] lista = new ElementoMultimediale[5];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Scegli il tipo (1.Audio, 2.Video, 3.Immagine): ");
+            int sceltaTipo = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (sceltaTipo) {
+                case 1:
+
+                    System.out.println("Inserisci il titolo");
+                    String titoloAudio = scanner.nextLine();
+                    System.out.println("Inserisci la durata");
+                    int durataAudio = scanner.nextInt();
+                    ElementoMultimediale elementoAudio = new RegistrazioneAudio(titoloAudio, durataAudio);
+                    lista[i] = elementoAudio;
+                    break;
+
+                case 2:
+
+                    System.out.println("Inserisci il titolo");
+                    String titoloVideo = scanner.nextLine();
+                    System.out.println("Inserisci la durata");
+                    int durataVideo = scanner.nextInt();
+                    System.out.println("Inserisci la luminosità");
+                    int luminositaVideo = scanner.nextInt();
+                    ElementoMultimediale elementoVideo = new Video(titoloVideo, durataVideo, luminositaVideo);
+                    lista[i] = elementoVideo;
+                    break;
+
+                case 3:
+
+                    System.out.println("Inserisci il titolo");
+                    String titoloImmagine = scanner.nextLine();
+                    System.out.println("Inserisci la luminosità");
+                    int luminositaImmagine = scanner.nextInt();
+                    ElementoMultimediale elementoImmagine = new Immagine(titoloImmagine, luminositaImmagine);
+                    lista[i] = elementoImmagine;
+                    break;
+                default:
+                    System.out.println("Scelta non valida. Riprova.");
+                    i--;
+                    break;
+
+            }
+        }
+
+        int riproduzione = 0;
+
+        do {
+            System.out.println("Scegli un elemento multimediale da 1 a 5, 0 per chiudere");
+            riproduzione = scanner.nextInt();
+            scanner.nextLine();
+            if (riproduzione > 5) {
+                System.out.println("scegli un valore tra 1 e 5, e 0 per chiudere");
+                riproduzione = scanner.nextInt();
+                scanner.nextLine();
+            }
+            if (riproduzione != 0) {
+                lista[riproduzione - 1].riproduci();
+            }
+        }
+        while (riproduzione > 0 && riproduzione <= 5);
     }
 }
