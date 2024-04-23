5ddec4eaa344523a0ee2ee37602c3a81ceffd3ba
GabScognamiglio <gabbo16s@gmail.com>
2024-04-23 16:10:56 +0200

    es giornaliero 1 e 2

--- UNIT_4_JAVA/Settimana_2/U4W2D2/untitled/src/Main.java
+++ UNIT_4_JAVA/Settimana_2/U4W2D2/untitled/src/Main.java
@@ -1,5 +1,7 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
 }
