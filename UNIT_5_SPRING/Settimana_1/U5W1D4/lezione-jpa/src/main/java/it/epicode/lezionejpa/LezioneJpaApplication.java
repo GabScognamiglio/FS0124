fc921e75572b9b401238c80eb8de6f233deeb5d6
GabScognamiglio <gabbo16s@gmail.com>
2024-05-16 09:55:12 +0200

    lezione jpa postgres in spring

---
+++ UNIT_5_SPRING/Settimana_1/U5W1D4/lezione-jpa/src/main/java/it/epicode/lezionejpa/LezioneJpaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.lezionejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezioneJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LezioneJpaApplication.class, args);
+	}
+
+}
---
+++ UNIT_5_SPRING/Settimana_1/U5W1D4/lezione-jpa/src/test/java/it/epicode/lezionejpa/LezioneJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.lezionejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezioneJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
