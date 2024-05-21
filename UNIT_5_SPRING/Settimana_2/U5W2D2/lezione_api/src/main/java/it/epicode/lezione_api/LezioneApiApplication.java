6633354f2e1c1b1f41df39b1283ce1ec1c2119ad
GabScognamiglio <gabbo16s@gmail.com>
2024-05-21 14:07:06 +0200

    ambiente es giornaliero 21/05

---
+++ UNIT_5_SPRING/Settimana_2/U5W2D2/lezione_api/src/main/java/it/epicode/lezione_api/LezioneApiApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.lezione_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezioneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LezioneApiApplication.class, args);
+	}
+
+}
