35419614d5cbecbe1f75cd0ab17ad978a4db04fb
GabScognamiglio <gabbo16s@gmail.com>
2024-05-23 17:16:52 +0200

    es giornaliero 23/05

--- UNIT_5_SPRING/Settimana_2/U5W2D4/esGiornaliero/es_giornaliero/src/main/java/it/epicode/es_giornaliero/dto/AutoreDto.java
+++ UNIT_5_SPRING/Settimana_2/U5W2D4/esGiornaliero/es_giornaliero/src/main/java/it/epicode/es_giornaliero/dto/AutoreDto.java
@@ -1,6 +1,7 @@
 package it.epicode.es_giornaliero.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -10,10 +11,10 @@ import java.time.LocalDate;
 
 @Data
 public class AutoreDto {
-    @NotNull
+    @NotBlank(message = "Il nome va inserito")
     @Size(max = 30)
     private String nome;
-    @NotNull
+    @NotBlank
     @Size(max = 30)
     private String cognome;
     @Email
