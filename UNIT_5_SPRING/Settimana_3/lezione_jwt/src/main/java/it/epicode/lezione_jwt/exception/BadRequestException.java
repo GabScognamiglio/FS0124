98dec8d993d44c0c6c443f13bf5784526f859332
GabScognamiglio <gabbo16s@gmail.com>
2024-05-27 14:06:18 +0200

    lezione jwt

--- UNIT_5_SPRING/Settimana_3/lezione_jwt/src/main/java/it/epicode/lezione_jwt/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package it.epicode.lezione_jwt.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- UNIT_5_SPRING/Settimana_3/lezione_jwt/src/main/java/it/epicode/lezione_jwt/exception/USerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package it.epicode.lezione_jwt.exception;
-
-public class USerNotFoundException extends RuntimeException{
-    public USerNotFoundException(String message) {
-        super(message);
-    }
-}
